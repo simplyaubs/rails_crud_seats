878963eddd3b887e071523c58969af64e2d18fe4
Aubrey Howell <simplyaubs@gmail.com>
2014-06-22 18:33:49 -0400

    User can update a seat from list.

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -15,6 +15,20 @@ class SeatsController < ApplicationController
     end
   end
 
+  def show
+    @seat = Seat.find(params[:id])
+  end
+
+  def edit
+    @seat = Seat.find(params[:id])
+  end
+
+  def update
+    @seat = Seat.find(params[:id])
+    @seat.update_attributes!(seat_params)
+
+    redirect_to seats_path
+  end
 
   private
   def seat_params
--- spec/features/seats_spec.rb
+++ spec/features/seats_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD bars' do
     expect(page).to have_content '36C'
     expect(page).to have_content 'Back'
   end
+
+  scenario 'User can update a seat from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a seat'
+    fill_in 'Name', with: '36C'
+    fill_in 'Location', with: 'Back'
+    click_on 'Add seat'
+    expect(page).to have_content '36C'
+    expect(page).to have_content 'Back'
+    click_on '36C'
+    expect(page).to have_content '36C'
+    expect(page).to have_content 'Back'
+    click_on 'Edit'
+    fill_in 'Name', with: '5A'
+    fill_in 'Location', with: 'Front'
+    click_on 'Update seat'
+    expect(page).to have_content '5A'
+    expect(page).to have_content 'Front'
+    expect(page).to_not have_content '36C'
+    expect(page).to_not have_content 'Back'
+  end
 end
